7b49828c0710837111e54ac4fd05f1869f52900e
Keturah Howard <keturah.dev@gmail.com>
2020-04-28 10:25:49 -0700

    create and fail test for form toggle functionality in reducer.
    Co-authored-by: Mariam <maralaeddin@gmail.com>
    Co-authored-by: Todd Walraven <cod4247@yahoo.com>
    Co-authored-by: acolistro <arcolistro@gmail.com>

--- src/__tests__/reducer/form-visible-reducer-test.js
+++ src/__tests__/reducer/form-visible-reducer-test.js
@@ -1,7 +1,11 @@
 import formVisibleReducer from '../../reducers/form-visible-reducer'
 
 describe("formVisibleReducer", () => {
+
   test("Should return the default state if no action type is enacted", () => {
-    expect(formVisibleReducer(false, {type:null})).toEqual(false)
+    expect(formVisibleReducer(false, {type: null})).toEqual(false)
   });
+  test("should toggle between current status of state", () => {
+    expect(formVisibleReducer(false, { type: "TOGGLE_FORM"})).toEqual(true)
+  })
 });
